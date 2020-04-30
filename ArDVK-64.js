1ec1cf164caf409558bd50bebb90a6d88d4e658f
Erick <erick.teixeira_50@hotmail.com>
2020-04-30 11:44:39 -0300

    Correção de bug

--- ArDVK-64.js
+++ ArDVK-64.js
@@ -8,6 +8,10 @@ function getHex(ent) {
   return Buffer.from(ent, 'utf8').toString('hex');
 }
 
+function hexToString(str) {
+  return Buffer.from(str, 'hex').toString('utf8');
+}
+
 function getBinary(string) {
   return string.split('').map(function (char) {
     let bin = char.charCodeAt(0).toString(2)
@@ -34,6 +38,7 @@ function getBlocks(ent) {
       count = 0;
     }
   }
+  blocks.push(block);
   let last_block = blocks[blocks.length-1];
   let zeros_added = 0;
   if (last_block.length < 6) {
@@ -57,7 +62,7 @@ function reverseString(str) {
    return str.split('').reverse().join('') 
 } 
 
-function encode(blocks_data) {
+function assemble(blocks_data) {
   let encoded = new String();
   blocks_data.blocks.forEach((block) => {
     encoded += pos[getDec(block)];
@@ -65,13 +70,58 @@ function encode(blocks_data) {
   return reverseString(encoded+blocks_data.zeros.toString());
 }
 
+function binaryToString(str) {
+  str = str.replace(/\s+/g, ''); //Remove spaces
+  str = str.match(/.{1,8}/g).join(" "); //Divides in bytes
+  var newBinary = str.split(" ");
+  var binaryCode = [];
+  for (i = 0; i < newBinary.length; i++) {
+    binaryCode.push(String.fromCharCode(parseInt(newBinary[i], 2)));
+  }   
+  return binaryCode.join("");
+}
 
+function disassemble(encoded) {
+  encoded = reverseString(encoded);
+  const zeros_added = Number(encoded[encoded.length-1]);
+  let blocks = new Array();
+  for (let i = 0; i < encoded.length-1; i++) {
+    let index = pos.indexOf(encoded[i]);
+    let binary = index.toString(2);
+    if (binary.length < 6) {
+      let dif = 6-binary.length;
+      let new_bin = new String();
+      for (let i = 0; i < dif; i++) new_bin += '0';
+      new_bin += binary;
+      blocks.push(new_bin);
+    }
+    else blocks.push(binary);
+  }
+  let bin = new String();
+  blocks.forEach((block) => {
+    bin += block;
+  });
+  if (zeros_added > 0){
+    for (let i = 0; i < zeros_added; i++) bin += '0'
+  }
+  return bin;
+}
 
-let msg = 'Ola'
+function encode(msg) {
+  let bin = getBinary(getHex(msg));
+  let blocks_return = getBlocks(bin);
+  let encoded = assemble(blocks_return);
+  return encoded;
+}
 
-let bin = getBinary(getHex(msg))
-console.log(bin);
-let blocks_return = getBlocks(bin);
-console.log(blocks_return);
-let encoded = encode(blocks_return);
-console.log(encoded)
+function decode(encoded) {
+  const bin = disassemble(encoded);
+  const hex = binaryToString(bin);
+  const dec = hexToString(hex);
+  return dec;
+}
+
+let enc = encode('loren ipsum');
+console.log('Encoded: ', enc);
+let dec = decode(enc);
+console.log('Decoded: ', dec);
